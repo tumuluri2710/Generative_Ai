4f22d058c2c442e8edff45861312fc38e14442ed
tumuluri2710 <lalithatumuluri1065@gmail.com>
2025-05-11 20:10:51 +0530

    first-huggingface-langchain

--- STREAM/README.md
+++
@@ -1,2 +0,0 @@
-# gemini-chatbot-starter
-Starter template for building a JS HTML chatbot
--- STREAM/response.py
+++
@@ -1,8 +0,0 @@
-def gen_res():
-    with  open('example.txt' ,'r') as f:
-        x=f.read()
-        return x
-        
-       
-print(str(gen_res()))
-   
